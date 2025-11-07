c4c22d2ea76b6b38aeaa9daa2b61e5582492f047
Adir Shitrit <adirshit212@gmail.com>
2025-11-07 18:09:06 +0200

    add thread enumeration support

--- ghost-core/src/lib.rs
+++ ghost-core/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod detection;
 pub mod memory;
 pub mod process;
+pub mod thread;
 
 pub use detection::{DetectionEngine, DetectionResult, ThreatLevel};
 pub use memory::{MemoryProtection, MemoryRegion};
 pub use process::ProcessInfo;
+pub use thread::ThreadInfo;
